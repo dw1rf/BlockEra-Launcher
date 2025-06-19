e5410e9c8eba91c8f772564965e7a5341ae7392b
didirus4 <DIDIRUS@yandex.ru>
2025-06-19 03:46:54 +0300

    fixed: unexpected error in crate visibility from minecraft_auth.rs

--- packages/app-lib/src/state/minecraft_auth.rs
+++ packages/app-lib/src/state/minecraft_auth.rs
@@ -571,7 +571,11 @@ const MICROSOFT_CLIENT_ID: &str = "00000000402b5328";
 const REDIRECT_URL: &str = "https://login.live.com/oauth20_desktop.srf";
 const REQUESTED_SCOPES: &str = "service::user.auth.xboxlive.com::MBI_SSL";
 
-struct RequestWithDate<T> {
+/* AstralRinth
+ * Weird visibility issue that didn't reproduce before
+ * Had to make DeviceToken and RequestWithDate pub(crate) to fix compilation error
+ */
+pub(crate) struct RequestWithDate<T> {
     pub date: DateTime<Utc>,
     pub value: T,
 }
